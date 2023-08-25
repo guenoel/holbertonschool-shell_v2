2370c1e30542c29daa0d595210ca6252ab7ebb5a
crasride <jose.dfernandez@yahoo.fr>
2023-08-25 16:33:36 +0200

    add search >

--- tokenizer.c
+++ tokenizer.c
@@ -2,18 +2,42 @@
 
 int tokenize_input(char *input, char *args[])
 {
-	int i = 0; /* Inicialización del contador de posiciones en el arreglo args */
-	char *token = strtok(input, " \t\n"); /* Obtener el primer token de la cadena input */
+	int i = 0;
+	char *token = strtok(input, " \t\n"); /* Obtén el primer token de la cadena input */
+	char *redir_token;
 
-	/* Ciclo para procesar todos los tokens */
 	while (token != NULL)
 	{
-		/* Duplicar el token actual y almacenarlo en el arreglo args */
-		args[i] = _strdup(token);
-		token = strtok(NULL, " \t\n"); /* Obtener el siguiente token */
-		i++; /* Incrementar el contador de posiciones */
+		/* Verifica si el token contiene un signo de redirección */
+		redir_token = _strchr(token, '>');
+		if (redir_token != NULL)
+		{
+			/* Si contiene '>', divídelo en dos tokens y almacena cada parte */
+			if (redir_token != token)
+			{
+				*redir_token = '\0'; /* Coloca '\0' en lugar de '>' */
+				args[i] = _strdup(token);
+				i++;
+			}
+			args[i] = _strdup(">");
+			i++;
+			redir_token++; /* Avanza el puntero después de '>' */
+			if (*redir_token != '\0')
+			{
+				args[i] = _strdup(redir_token);
+				i++;
+			}
+		}
+		else
+		{
+			/* Duplica el token actual y almacénalo en el arreglo args */
+			args[i] = _strdup(token);
+			i++;
+		}
+
+		token = strtok(NULL, " \t\n"); /* Obtén el siguiente token */
 	}
 
-	args[i] = NULL; /* Marcar el final del arreglo args con un puntero nulo */
-	return (i); /* Devolver la cantidad de tokens procesados */
+	args[i] = NULL; /* Marca el final del arreglo args con un puntero nulo */
+	return i;
 }
