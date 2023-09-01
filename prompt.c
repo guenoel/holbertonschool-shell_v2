841e23aa37ea8f68c1cad78bc34b1a29613d0c4f
crasride <105241291+crasride@users.noreply.github.com>
2023-09-01 10:14:10 +0200

    Update prompt.c

--- prompt.c
+++ prompt.c
@@ -70,12 +70,6 @@ int run_shell_loop(void)
 	int num_args = 0;
 	int line_number = 0;
 	int status = 0;
-	char *ptr_found = NULL;
-	/* char *ptr_found2 = NULL; */
-	int num_line_heredoc = 0;
-	char *delim = "never_choose_this_delim";
-	char *cropped_input = NULL;
-	/* char *dup_input = NULL; */
 
 	while (1) /* Bucle infinito para mantener la shell en funcionamiento */
 	{
@@ -85,20 +79,7 @@ int run_shell_loop(void)
 		{
 			printf("$ "); /* indicador de línea ($) solo en modo interactivo */
 		}
-		/* printf("--- Line: %d ---\n", line_number); */
-
 		input = read_input(); /* Leer la línea de entrada */
-
-		/* printf("input: %s\n", input); */
-		
-		/* condition d entrée heredoc */
-		ptr_found = _strchr(input, '<');
-		/* printf("-----ici-----\n"); */
-		if (ptr_found != NULL)
-			if (ptr_found[1] == '<')
-			{
-				num_line_heredoc = line_number;
-			}
 
 		if (input == NULL)
 		{
@@ -115,42 +96,8 @@ int run_shell_loop(void)
 			continue; /* Línea vacía, volver al inicio del bucle */
 		}
 
-		/* condition sortie heredoc */
-		if (input != NULL)
-		{
-			/* dup_input = _strdup(input);
-			ptr_found2 = _strchr(dup_input, '\n');
-			
-			if (ptr_found2 != NULL) */
-
-				cropped_input = strtok(input, "\n");
-			/* free(dup_input); */
-			
-			if(cropped_input)
-			{
-				/* printf("cropped:%s delim:%s\n", cropped_input, delim); */
-				if (_sstrcmp(cropped_input, delim) == 0)
-				{
-					num_line_heredoc = 0;
-					/* free(delim); */
-				}
-			}
-		}
-
-
 		num_args = tokenize_input(input, args); /* Tokenizar la línea de entrada */
 
-		/* recupération du délimliteur */
-		if (line_number == num_line_heredoc)
-		{
-			delim = _strdup(args[2]);
-		}
-		/* printf("num_line_heredoc: %d line_num: %d\n", num_line_heredoc, line_number); */
-		if ((num_line_heredoc != 0 || (_sstrcmp(cropped_input, delim) == 0)) && num_line_heredoc != line_number )
-		{
-			/* printf("continue!!!\n"); */
-			continue;
-		}
 		if (num_args == 0)
 		{
 			free(input);
@@ -195,6 +142,5 @@ int run_shell_loop(void)
 		free(input); /* Liberar la memoria de la línea de entrada */
 		free_args(args); /* Liberar la memoria de los argumentos tokenizados */
 	}
-	free(delim);
 	return (status);
 }
