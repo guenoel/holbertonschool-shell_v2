b7904e5709bb3395e79c816ad14aa60daed27285
crasride <105241291+crasride@users.noreply.github.com>
2023-09-03 20:27:34 +0200

    Update functions_env.c

--- functions_env.c
+++ functions_env.c
@@ -1,5 +1,7 @@
 #include "shell.h"
 
+#define UNUSED(x) (void)(x)
+
 /**
  * get_env_var - Retrieves the value of an environment variable.
  * @name: The name of the environment variable.
@@ -24,78 +26,26 @@ char *get_env_var(const char *name)
 }
 
 /**
- * count_env_var - set variable in environement
- * @env: pointer to extern environement variable
- * @args: An array of arguments passed to the 'unsetenv' command.
- * @flag_var_env_found: flag to 1 if environement variable already exist
+ * shell_env - Display the environment variables.
+ * @args: An array of arguments (not used).
  *
- * Return: number of variables
+ * Return: 1 (success).
+ *
+ * This function displays all environment variables to the standard output.
  */
-int count_env_var(char **env, char *args[], int *flag_var_env_found)
+int shell_env(char *args[])
 {
-	int num_vars = 0;
-	char *tmp = NULL;
-	char *tmp2 = NULL;
+	char **env = environ; /* Obtener el arreglo de variables de entorno */
 
+	UNUSED(args);
 	while (*env)
 	{
-		tmp = _strdup(*env);
-		tmp2 = strtok(tmp, "=");
-		if (_sstrcmp(tmp2, args[1]) == 0)
-			*flag_var_env_found = 1;
-		free(tmp);
-		num_vars++;
+		printf("%s\n", *env); /* Imprimir cada variable de entorno */
 		env++;
 	}
-	env = environ;
-	return (num_vars);
+	return (1); /* Indicar que el comando se ejecutó correctamente */
 }
 
-/**
- * set_env_var - set variable in environement
- * @env: pointer to extern environement variable
- * @args: An array of arguments passed to the 'unsetenv' command.
- * @size_malloc: size of new environement
- * @new_env_var: name of new variable
- * @flag_var_env_found: flag to 1 if environement variable already exist
- *
- * Return: status 0 on success, 1 on failure.
- */
-int set_env_var(char **env, char *args[], int size_malloc,
-char *new_env_var, int flag_var_env_found)
-{
-	char **new_environ = NULL;
-	int i = 0, status = 0;
-	char *tmp = NULL;
-	char *tmp2 = NULL;
-
-	new_environ = (char **)malloc((size_malloc) * sizeof(char *));
-	init_array_of_strings(new_environ, (size_malloc));
-	if (new_environ == NULL)
-	{
-		perror("malloc"); /* Mostrar error si la asignación de memoria falla */
-		status = 1;
-		return (status);
-	}
-	while (*env)
-	{
-		tmp = _strdup(*env);
-		tmp2 = strtok(tmp, "=");
-		if (_sstrcmp(tmp2, args[1]) == 0)
-			new_environ[i] = new_env_var;
-		else
-			new_environ[i] = _strdup(*env);
-		free(tmp);
-		i++;
-		env++;
-	}
-	if (!flag_var_env_found)
-	{
-		new_environ[i] = new_env_var;/* add new var de entorno al arreglo */
-		i++;
-	}
-	return (status);
-}
 /**
  * shell_unsetenv - Remove an environment variable.
  * @args: An array of arguments passed to the 'unsetenv' command.
@@ -107,21 +57,38 @@ char *new_env_var, int flag_var_env_found)
  */
 int shell_unsetenv(char *args[])
 {
+	int flag_var_env_found = 0;
 	char **new_environ = NULL;
 	char **env = environ;
 	char *tmp = NULL;
 	char *tmp2 = NULL;
-	int i = 0, num_vars = 0, num_args = 0, flag_var_env_found = 0;
+	int i = 0;
+	int num_vars = 0;
+	int num_args = 0;
 
 	while (args[num_args] != NULL)
 		num_args++;
 	if (num_args == 1)
 		return (0);
 
-	num_vars = count_env_var(env, args, &flag_var_env_found);
+/* STEP 1 - PREPARACION DEL DATA, DE num_vars Y DEL FLAG*/
+	while (*env)
+	{
+		tmp = _strdup(*env);
+		tmp2 = strtok(tmp, "=");
+		if (_sstrcmp(tmp2, args[1]) == 0)
+			flag_var_env_found = 1;
+		free(tmp);
+		num_vars++;
+		env++;
+	}
 	if (flag_var_env_found)
 	{
+		/* re-init env to the start */
+		char **env = environ;
+/* STEP 2 CREACION DEL NEW ENV WITH A VARIABLE LESS */
 		new_environ = (char **)malloc((num_vars) * sizeof(char *));
+/* STEP 3 COPY FROM OLD TO NEW ENV*/
 		while (*env)
 		{
 			tmp = _strdup(*env);
@@ -136,9 +103,12 @@ int shell_unsetenv(char *args[])
 			env++;
 		}
 		new_environ[i] = NULL;
+		/* STEP 4 FREE OLD ENV*/
+		/* re-init env to the start */
 		env = environ;
 		free_args(env);
 		free(env);
+/* STEP 5 ASIGNACION DE NUEVO ENVIRON */
 		environ = new_environ;
 	} else
 	{
@@ -160,13 +130,19 @@ int shell_unsetenv(char *args[])
  */
 int shell_setenv(char *args[])
 {
-	int flag_var_env_found = 0, size_malloc = 0, num_vars = 0, status = 0;
-	char **env = environ; /* Obtener arreglo de variables entorno existentes */
 	char **new_environ = NULL;
+	int flag_var_env_found = 0;
+	char **env = environ; /* Obtener arreglo de variables entorno existentes */
 	char *new_env_var = NULL;
+	int num_vars = 0;
+	int size_malloc = 0;
+	char *tmp = NULL;
+	char *tmp2 = NULL;
+	int i = 0;
 
 	if (args[1] != NULL && args[2] != NULL) /* si se dan suficientes argumentos */
 	{
+		/* STEP 1 - PREPARACION DEL DATA, DE num_vars Y DEL FLAG*/
 		size_malloc = _strlen(args[1]) + _strlen(args[2]) + 2;
 		new_env_var = (char *)malloc(size_malloc); /* 2: "=" + "\0" */
 		init_string(new_env_var, size_malloc);
@@ -177,24 +153,62 @@ int shell_setenv(char *args[])
 			return (1);
 		}
 		sprintf(new_env_var, "%s=%s", args[1], args[2]); /* hacer cadena env */
-		num_vars = count_env_var(env, args, &flag_var_env_found);
+
+		while (*env)
+		{
+			tmp = _strdup(*env);
+			tmp2 = strtok(tmp, "=");
+			if (_sstrcmp(tmp2, args[1]) == 0)
+				flag_var_env_found = 1;
+			free(tmp);
+			num_vars++; /* Contar el número de variables de entorno existentes */
+			env++;
+		}
 		env = environ;/* Reiniciar el puntero al arreglo de variables de entorno */
+
+		/* STEP 2 CREACION DEL NEW ENV */
 		if (flag_var_env_found)
 			size_malloc = num_vars + 1;
 		else
 			size_malloc = num_vars + 2;
-		set_env_var(env, args, size_malloc, new_env_var, flag_var_env_found);
+		new_environ = (char **)malloc((size_malloc) * sizeof(char *));
+		init_array_of_strings(new_environ, (size_malloc));
+		if (new_environ == NULL)
+		{
+			perror("malloc"); /* Mostrar error si la asignación de memoria falla */
+			return (1);
+		}
+/* STEP 3 COPY FROM OLD TO NEW ENV*/
 
+		while (*env)
+		{
+			tmp = _strdup(*env);
+			tmp2 = strtok(tmp, "=");
+			if (_sstrcmp(tmp2, args[1]) == 0)
+				new_environ[i] = new_env_var;
+			else
+				new_environ[i] = _strdup(*env);
+			free(tmp);
+			i++;
+			env++;
+		}
+		if (!flag_var_env_found)
+		{
+			new_environ[i] = new_env_var;/* add new var de entorno al arreglo */
+			i++;
+		}
+
+/* STEP 4 FREE OLD ENV*/
 		env = environ;/* Reiniciar el puntero al arreglo de variables de entorno */
 		free_args(env);
 		free(env);
+/* STEP 5 ASIGNACION DE NUEVO ENVIRON */
 		environ = new_environ;/* Act variable global 'env' reflejar nuevo arreglo */
 	}
 	else
 	{
 		fprintf(stderr, "Usage: setenv VARIABLE_NAME VALUE\n");
-		status = 1;
-		return (status);
+		return (1);
 	}
-	return (status);
+	return (0);
 }
