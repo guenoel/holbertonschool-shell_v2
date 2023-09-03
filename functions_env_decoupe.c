25332f9750b8f51ab23884eb42fb4d609a064d38
guenoel <guenooel@hotmail.com>
2023-09-03 14:00:02 -0700

    test bettyfy function_env

--- functions_env_decoupe.c
+++ functions_env_decoupe.c
@@ -47,6 +47,7 @@ int count_env_var(char **env, char *args[], int *flag_var_env_found)
 		num_vars++;
 		env++;
 	}
+	env = environ;
 	return (num_vars);
 }
 
@@ -95,7 +96,6 @@ char *new_env_var, int flag_var_env_found)
 	}
 	return (status);
 }
-
 /**
  * shell_unsetenv - Remove an environment variable.
  * @args: An array of arguments passed to the 'unsetenv' command.
@@ -117,11 +117,11 @@ int shell_unsetenv(char *args[])
 		num_args++;
 	if (num_args == 1)
 		return (0);
+
 	num_vars = count_env_var(env, args, &flag_var_env_found);
+
 	if (flag_var_env_found)
 	{
-		char **env = environ;
-
 		new_environ = (char **)malloc((num_vars) * sizeof(char *));
 		while (*env)
 		{
@@ -161,10 +161,10 @@ int shell_unsetenv(char *args[])
  */
 int shell_setenv(char *args[])
 {
-	char **new_environ = NULL;
+	int flag_var_env_found = 0, size_malloc = 0, num_vars = 0, status = 0;
 	char **env = environ; /* Obtener arreglo de variables entorno existentes */
+	char **new_environ = NULL;
 	char *new_env_var = NULL;
-	int num_vars = 0, size_malloc = 0, flag_var_env_found = 0, status = 0;
 
 	if (args[1] != NULL && args[2] != NULL) /* si se dan suficientes argumentos */
 	{
@@ -174,19 +174,23 @@ int shell_setenv(char *args[])
 		size_malloc = 0;
 		if (new_env_var == NULL)
 		{
-			perror("malloc");
-			status = 1;
-			return (status);
+			perror("malloc");/* Mostrar error si la asignación de memoria falla */
+			return (1);
 		}
 		sprintf(new_env_var, "%s=%s", args[1], args[2]); /* hacer cadena env */
-		num_vars = count_env_var(env, args, &flag_var_env_found);
+
+	num_vars = count_env_var(env, args, &flag_var_env_found);
+
 		env = environ;/* Reiniciar el puntero al arreglo de variables de entorno */
 		if (flag_var_env_found)
 			size_malloc = num_vars + 1;
 		else
 			size_malloc = num_vars + 2;
-		status = set_env_var(env, args, size_malloc, new_env_var,
-							flag_var_env_found);
+
+	status = set_env_var(env, args, size_malloc, new_env_var, flag_var_env_found);
+		if (status != 0)
+			return status;
+
 		env = environ;/* Reiniciar el puntero al arreglo de variables de entorno */
 		free_args(env);
 		free(env);
@@ -198,5 +202,5 @@ int shell_setenv(char *args[])
 		status = 1;
 		return (status);
 	}
-	return (0);
+	return (status);
 }
