d515b4eba4e51bc115cc5cfccd6c60a3d74cb25f
crasride <105241291+crasride@users.noreply.github.com>
2023-09-03 20:23:42 +0200

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -1,17 +1,33 @@
 #include "shell.h"
 
 /**
-* tokenize_output - Tokenizes an input string into an array of arguments,
+* tokenize_input - Tokenizes an input string into an array of arguments,
 * handling redirection.
+* @input: The input string to tokenize.
 * @args: An array to store the resulting arguments.
-* @redir_token: string of redirection sign
-* @token: string token from line command
+*
+* Return: The number of arguments parsed.
+*
+* This function splits the input string into individual tokens, separating
+* them by spaces, tabs, and newlines.
+* It also handles input and output redirection tokens ("<", ">", ">>", "<<"),
+* splitting them as separate arguments.
+* The resulting tokens are stored in the 'args' array, and the function returns
+* the number of arguments parsed.
 */
-void tokenize_output(char *args[], char *redir_token, char *token)
+int tokenize_input(char *input, char *args[])
 {
 	int i = 0;
+	char *token = strtok(input, " \t\n"); /* Obtén primer token cadena input */
+	char *redir_token;
 
-	/* contiene '>', verifica si '>>' o solo '>', y divide secuencia */
+	while (token != NULL)
+	{
+		/* Verifica el token contiene signo redirección de salida '>' o '>>' */
+		redir_token = _strchr(token, '>');
+		if (redir_token != NULL)
+		{
+			/* contiene '>', verifica si '>>' o solo '>', y divide secuencia */
 			if (redir_token != token)
 			{
 				*redir_token = '\0'; /* Coloca '\0' en lugar de '>' */
@@ -35,90 +51,18 @@ void tokenize_output(char *args[], char *redir_token, char *token)
 				args[i] = _strdup(redir_token);
 				i++;
 			}
-}
-
-/**
-* tokenize_input - Tokenizes an input string into an array of arguments,
-* handling redirection.
-* @args: An array to store the resulting arguments.
-* @redir_token: string of redirection sign
-* @token: string token from line command
-*/
-void tokenize_input(char *args[], char *redir_token, char *token)
-{
-	int i = 0;
-
-	/* Si contiene '<', verifica si es solo '<', y divide en consecuencia */
-	if (redir_token != token)
-	{
-		*redir_token = '\0'; /* Coloca '\0' en lugar de '<' */
-		args[i] = _strdup(token);
-		i++;
-	}
-	args[i] = _strdup("<");
-	i++;
-	redir_token++; /* Avanza el puntero después de '<' */
-	if (*redir_token != '\0')
-	{
-		args[i] = _strdup(redir_token);
-		i++;
-	}
-}
-
-/**
-* tokenize_double_output - Tokenizes an input string
-* into an array of arguments,
-* handling redirection.
-* @args: An array to store the resulting arguments.
-* @token: string token from line command
-*/
-void tokenize_double_output(char *args[], char *token)
-{
-	int i = 0;
-
-	/* Si contiene '<<', es una redirección de heredoc */
-	args[i] = _strdup("<<");
-	i++;
-	token = strtok(NULL, " \t\n"); /* Obtén el delimitador del heredoc */
-	if (token != NULL)
-	{
-		args[i] = _strdup(token);
-		i++;
-	}
-}
-
-/**
-* tokenize_command - Tokenizes an input string into an array of arguments,
-* handling redirection.
-* @input: The input string to tokenize.
-* @args: An array to store the resulting arguments.
-*
-* Return: The number of arguments parsed.
-*
-* This function splits the input string into individual tokens, separating
-* them by spaces, tabs, and newlines.
-* It also handles input and output redirection tokens ("<", ">", ">>", "<<"),
-* splitting them as separate arguments.
-* The resulting tokens are stored in the 'args' array, and the function returns
-* the number of arguments parsed.
-*/
-int tokenize_command(char *input, char *args[])
-{
-	int i = 0;
-	char *token = strtok(input, " \t\n"); /* Obtén primer token cadena input */
-	char *redir_token;
-
-	while (token != NULL)
-	{
-		/* Verifica el token contiene signo redirección de salida '>' o '>>' */
-		redir_token = _strchr(token, '>');
-		if (redir_token != NULL)
-		{
-			tokenize_output(args, redir_token, token);
 		}
 		else if (_sstrcmp(token, "<<") == 0)
 		{
-			tokenize_double_output(args, token);
+			/* Si contiene '<<', es una redirección de heredoc */
+			args[i] = _strdup("<<");
+			i++;
+			token = strtok(NULL, " \t\n"); /* Obtén el delimitador del heredoc */
+			if (token != NULL)
+			{
+				args[i] = _strdup(token);
+				i++;
+			}
 		}
 		else
 		{
@@ -126,7 +70,21 @@ int tokenize_command(char *input, char *args[])
 			redir_token = _strchr(token, '<');
 			if (redir_token != NULL)
 			{
-				tokenize_input(args, redir_token, token);
+				/* Si contiene '<', verifica si es solo '<', y divide en consecuencia */
+				if (redir_token != token)
+				{
+					*redir_token = '\0'; /* Coloca '\0' en lugar de '<' */
+					args[i] = _strdup(token);
+					i++;
+				}
+				args[i] = _strdup("<");
+				i++;
+				redir_token++; /* Avanza el puntero después de '<' */
+				if (*redir_token != '\0')
+				{
+					args[i] = _strdup(redir_token);
+					i++;
+				}
 			}
 			else
 			{
